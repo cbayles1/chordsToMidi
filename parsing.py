05cb22990446359c6d3fb71d849a456e95c19026
Caleb <clbayles@hotmail.com>
2023-05-27 18:50:14 -0500

    converted from midiutil to mingus

--- parsing.py
+++ parsing.py
@@ -1,44 +1,22 @@
 import re
-from midiutil import MIDIFile
-
-class Note:
-    def __init__(self, midiNote,  time, track=0, channel=0, duration=1, volume=100):    
-        self.midiNote = midiNote
-        self.track = track
-        self.channel = channel
-        self.time = time
-        self.duration = duration
-        self.volume = volume
+from mingus.containers import Track
+from mingus.containers import Bar
+from mingus.containers import NoteContainer
+from mingus.midi import midi_file_out
 
 def getChords(filepath):
     with open(filepath, 'rt', encoding="utf-8") as f:
         content = f.read()
-        # Split the content
-        data_list = re.split(r'\s+|\\n', content)
-        # Remove any empty elements from the list
-        data_list = list(filter(None, data_list))
+        data_list = re.split(r'\s+|\\n', content) #split on newlines and spaces
+        data_list = list(filter(None, data_list))  # Remove any empty elements from the list
         return data_list
 
 def getRoot(chord):
     if len(chord) > 1 and (chord[1] == '#' or chord[1] == 'b'):
         return chord[0:2]
     return chord[0]
-
-def parseChord(chord):
-    root = getRoot(chord)
-    notes = []
-    
-def exportNotesToMidiFile(notes, tempo):
-    midiFile = MIDIFile(1)
-    midiFile.addTempo(0, 0, tempo)
-    
-    for note in notes:
-        midiFile.addNote(note.track, note.channel, note.midiNote, note.time, note.duration, note.volume)
-
-    with open("output.mid", "wb") as f:
-        midiFile.writeFile(f)
      
-def parseAllChords(chords):
+def convertTildes(chords):
     for i in range(0, len(chords)):
         chord = chords[i]
         if chord[0] == '~':
@@ -47,9 +25,18 @@ def parseAllChords(chords):
             else:
                 chords[i] = chords[i-1]
                 chord = chords[i-1]
-        parseChord(chord)
+
+def chordsToTrack(chords):
+    t = Track()
+    for chord in chords:
+        nc = NoteContainer(chord)
+        b = Bar()
+        b.add_notes(chord)
+        t.add_bar()
+    return t
 
 #------------------------------------MAIN------------------------------------
 chords = getChords('input.txt')
-parseAllChords(chords)
-exportNotesToMidiFile(80)
+convertTildes(chords)
+track = chordsToTrack(chords)
+midi_file_out.write_Track("test.mid", track)
